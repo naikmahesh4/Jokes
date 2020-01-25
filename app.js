faf8792e29b218a02447217d970a7ef5a6050d66
mahesh <naik.mahesh575@gmail.com>
2020-01-25 16:14:31 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+document.querySelector('.get-jokes').addEventListener('click',getJokes);
+
+function getJokes(e){
+  console.log('get jokes');
+
+  const number = document.querySelector('input[type="number"]').value;
+
+ const xhr = new XMLHttpRequest();
+
+ xhr.open('GET',`http://api.icndb.com/jokes/random/${number}`,true);
+
+ xhr.onload = function(){
+   if(this.status === 200){
+     const response = JSON.parse(this.responseText);
+     console.log(response);
+   
+     output = '';
+
+    if(response.type === 'success'){
+      response.value.forEach(function(jokes){
+        output += `<li>${jokes.joke}</li>`
+      })
+
+    }else{
+      output += `<li>Something Went wrong</li>`
+    }
+  }
+  document.querySelector('.jokes').innerHTML = output;
+ }
+
+ xhr.send();
+
+  e.preventDefault();
+}
